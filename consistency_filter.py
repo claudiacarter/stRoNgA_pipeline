5fdc817e4a963c7ad0559e13051cb4526ed38942
claudiacarter <cloudycarter@gmail.com>
2024-01-14 23:37:15 +0000

    added run time and retrieval of files from input directory
    Signed-off-by: claudiacarter <cloudycarter@gmail.com>

--- consistency_filter.py
+++ consistency_filter.py
@@ -3,27 +3,27 @@
 # aligned to host or microbe genome. Output is one FASTQ containing only the sRNAs with
 # exact matches in every sample dataset.
 
-# This version searches each sRNA found in any sample against each of the samples' sRNA sequences
-# and only retains a list of those found with an exact match in every sample. Further refinement
-# could include non-exact matches, especially those with an exact match but within a longer sequence.
+# The approach of this version is to only retains a list of those found with an exact match in every
+# sample through finding the intersection of sets of sRNAs created from each sample's fastq. Further refinement
+# could include non-exact matches, especially those with an exact match but within a longer sequence - if necessary.
+
+import pathlib
+import time
 
 from Bio import SeqIO
 
-# open FASTQs and parse into a list of sets (i.e. list of x samples each containing set of y sRNAs)
+start_time = time.time()
 
-# def read_fq_file(file_path):
-#    with open(file_path, 'r') as fq_file:
-#        lines = fq_file.readlines()
-#        return [(lines[i].strip(), lines[i+1].strip(), lines[i+3].strip()) for i in range(0, len(lines), 4)]
-test_sample_1 = "testsample1.fq"
-test_sample_2 = "testsample2.fq"
-test_sample_3 = "testsample3.fq"
-sample_file_names = [test_sample_1, test_sample_2, test_sample_3]
+# iterate through directory containing input files to find files and add paths to a list
+input_dir = r"C:\Users\clauc\PycharmProjects\pythonProject\input_fqs"
+sample_file_names = [f for f in pathlib.Path(input_dir).iterdir()]
 
+
+# open input files and parse into a list of sets (i.e. list of x samples each containing set of y sRNAs)
 total_seqs = []
-for sample in sample_file_names:
+for sample_file in sample_file_names:
     seqs = []
-    for record in SeqIO.parse(sample, "fastq"):
+    for record in SeqIO.parse(sample_file, "fastq"):
         seqs.append(record.seq)
     total_seqs.append(set(seqs))
 # print(total_seqs)
@@ -31,6 +31,11 @@ for sample in sample_file_names:
 # Find the intersection of all sample sets (i.e.only sRNAs in ALL)
 
 consistent_seqs = total_seqs[0].intersection(*total_seqs)
-print(consistent_seqs)
+# print(consistent_seqs)
+print(len(consistent_seqs))
+
 
 # Write back to fasta/fastq, maybe include Abundance Ranking in here to return results in RPM order?
+end_time = time.time()
+
+print("Time taken: ", end_time - start_time, "seconds")
