1c5b598ed850fd0c360c76c8a6bc9b7abeea0c4a
claudiacarter <cloudycarter@gmail.com>
2024-02-22 17:39:58 +0000

    code cleanup
    Signed-off-by: claudiacarter <cloudycarter@gmail.com>

--- consistency_filter.py
+++ consistency_filter.py
@@ -6,7 +6,6 @@
 # sample through finding the intersection of sets of sRNAs created from each sample's fastq. Further refinement
 # could include non-exact matches, especially those with an exact match but within a longer sequence - if necessary.
 
-
 import pathlib
 import time
 
@@ -15,7 +14,7 @@ from Bio import SeqIO
 start_time = time.time()
 
 # iterate through directory containing input files to find files and add paths to a list
-input_dir = r"C:\Users\clauc\PycharmProjects\pythonProject\input_fqs"
+input_dir = r"C:\Users\clauc\PycharmProjects\pythonProject\consistency_input_fqs"
 sample_file_names = [f for f in pathlib.Path(input_dir).iterdir()]
 
 quant_input = len(sample_file_names)
@@ -30,25 +29,14 @@ for sample_file in sample_file_names:
 # print(total_seqs)
 
 # Find the intersection of all sample sets (i.e.only sRNAs in ALL)
-
 consistent_seqs = total_seqs[0].intersection(*total_seqs)
 min_seq = len(min(consistent_seqs))
 max_seq = len(max(consistent_seqs))
 
 print("Number of common sequences across all", quant_input, "input files: ", len(consistent_seqs))
 print("Sequence lengths range from: ", min_seq, " to ", max_seq)
-# print(consistent_seqs)
 
-# how to include abundance ranking?
-# use sequence as a search term to retrieve RPM numbers from each sample input
-# assign each the average of the numbers retrieved
-# order by RPM highest to lowest
-
-# could write to a csv containing (per line) seq and average RPM, then RPMs and identifiers from each sample source or
 # Write back to fasta
-# will have to write new identifier lines as there would be multiple from original fastqs?
-# f"srna_target_candidate {}
-
 with open("output.fa", 'w') as output:
     for seq in enumerate(consistent_seqs):
         output.write(">" + f"srna_target_candidate_{seq[0]+1}" + "\n" + seq[1] + "\n")
